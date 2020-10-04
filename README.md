ab396a131759b4c409f4c26d7610b4a91cef6e4e
V4anderson <vandersonandroid321@gmail.com>
2020-10-04 11:08:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Exercicie_Word_Size_In_The_books
+ Programa para contar e mostrar para o usuario a quantidade de palavras em um determinado livro que esteja no formato ( .txt )
---
+++ main.py
@@ -0,0 +1,12 @@
+def word_count():
+    opcao = 1
+    while opcao == 1:
+        file_name = input("Digite o nome do livro: ")
+        try:
+            with open(file_name+'.txt') as book:
+                book = len(book.readlines())
+                print(" O Livro pesquisado contem aproximadamente "+str(book)+" Palavras\n")
+
+        except FileNotFoundError:
+            print("Livro não encontrado!\n")
+        opcao = int(input("1 - Para Pesquisar novamente: \n2 - Para encerrar o programa: "))
